c75c09312a530a4b22d7ea8ac5ba40da445e76bf
fledidev <admin@fledi.dev>
2023-10-30 23:06:53 +0100

    Added basic layout

---
+++ src/app/players/[name]/layout.tsx
@@ -0,0 +1,15 @@
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body className={montserrat.className}>{children}</body>
+      </html>
+    )
+  }
---
+++ src/app/players/[name]/page.tsx
@@ -0,0 +1,37 @@
+export default function Page({ params }: { params: { name: string } }) {
+  return (
+    <>
+      <main className="inline-flex min-h-screen w-screen flex-col items-center justify-start gap-8 bg-slate-950 px-5 pb-8 pt-5 ">
+        <div className="inline-flex items-center justify-center gap-3 self-stretch rounded bg-sky-950 px-6 py-3 lg:bg-transparent lg:py-6 ">
+          <img className="h-20 w-20" src="/avatar.png" />
+          <div className="inline-flex flex-col items-center justify-center gap-1">
+            <p className="text-3xl font-normal text-white">zGxd</p>
+            <p className="text-xl font-normal text-yellow-400">Squad Yellow</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-start justify-start gap-1 self-stretch rounded bg-sky-950 px-1.5 py-2 lg:self-center lg:px-6">
+          <div className="flex flex-col items-start justify-start gap-1.5 px-1.5 py-1">
+            <h2 className="text-xl font-normal text-zinc-500">Information</h2>
+          </div>
+          <div className="flex flex-col items-start justify-start gap-1.5 px-1.5 py-2">
+            <p className="text-xl text-white">
+              <span className="font-semibold">Kills: </span>21
+            </p>
+            <p className="text-xl text-white">
+              <span className="font-semibold">Deaths: </span>3
+            </p>
+            <p className="text-xl text-white">
+              <span className="font-semibold">Last Login: </span>30.10.23 22:00
+            </p>
+            <p className="text-xl text-white">
+              <span className="font-semibold">Current XP: </span>Level 34
+            </p>
+            <p className="text-xl text-white">
+              <span className="font-semibold">Playtime: </span>4 hours
+            </p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
